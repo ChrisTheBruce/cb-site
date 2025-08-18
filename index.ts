fd1e13e1d3b133f6bb3f63dcf74dbe6386dc81a1
Chris Brighouse <chris.brighouse@hotmail.co.uk>
2025-08-18 12:18:47 +0100

    index ts

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 // index.ts
 export default {
-    async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-        return new Response("Hi no Hono now", {
-            headers: {"content-type": "text/plain"},
-        });
-    },
-};
+    async fetch(request: Request, env: Env) {
+        const url = new URL (request.url);
+        return env.ASSETS.fetch(request);
+    }
+}
