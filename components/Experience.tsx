c1847faa2fb01fc7817b46483ac4e6b7896ced73
Chris Brighouse <chris.brighouse@hotmail.co.uk>
2025-08-19 15:42:17 +0100

    career

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -11,7 +11,7 @@ const experienceData: ExperienceItem[] = [
   },
   {
     role: "Director of Product Management, Energy & Engineering Solutions",
-    company: "OpenText, Reading, UK",
+    company: "OpenText, Reading, UK (aquired EMC's Documentum division)",
     period: "Feb 2012 - Jun 2023",
     description: "Led the product team creating innovative engineering business solutions. Produced more than $100M in industry-related income. Generated multimillion-dollar agreements and increased productivity while reducing customer costs by 50%."
   },
@@ -20,6 +20,30 @@ const experienceData: ExperienceItem[] = [
     company: "Sword CTSpace, London, UK",
     period: "Jan 2011 - Feb 2012",
     description: "Supervised successful delivery of consulting services through effective customer engagements. Boosted revenue 30% by overseeing professional services team's growth and reduced time-to-value for consumers by 20%."
+  },
+  {
+    role: "Principal Consultant, EMC, London, UK.  ",
+    company: "EMC (Documentum Division)",
+    period: "2006 to 2011",
+    description: "Strategic Consulting on Information Management Roadmaps for Fortune 500 companies companies.  Lead Consultant on large scale implementations of the Documentum ECM system with extensions for Engineering.in the Energy, Utilities and Construction Industries."
+  },
+  {
+    role: "Founder & CEO",
+    company: "Jario Ltd",
+    period: "2004 to 2006",
+    description: "Government funded startup to track document content within and across organizations"
+  },
+   {
+    role: "Co-Founder & CTO",
+    company: "DataCAD, CADspace, McLaren (series of acquisitions)",
+    period: "1995 to 2004",
+    description: "Developed Documentum-based engineering add-on solutions"
+  },
+  {
+    role: "Senior Consultant",
+    company: "Intergraph (UK)",
+    period: "1990 to 1995",
+    description: "Focussing primarily on Plant Design and Information Management solutions for Oil & Gas"
   }
 ];
 
