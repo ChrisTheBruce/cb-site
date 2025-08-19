0a15193e96d4c812facef0ab560994d2154daf6a
Chris Brighouse <chris.brighouse@hotmail.co.uk>
2025-08-19 11:50:40 +0100

    contact

--- main.jsx
+++ main.jsx
@@ -1,16 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import Contact from './components/Contact';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <Contact />
+  </StrictMode>
+);
 
-const el = document.getElementById('contact-root');
-if (el) {
-  createRoot(el).render(<Contact />);
-}
