c1a26e39cbb4320265da5e39c5416c2220ee03ff
Chris Brighouse <chris.brighouse@hotmail.co.uk>
2025-08-19 10:56:02 +0100

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./**/*.{html,js,jsx,ts,tsx}'],
+  theme: { extend: {} },
+  plugins: [],
+  // Optional: if you use dynamic class names, add a safelist:
+  // safelist: ['aspect-video','bg-gradient-to-tr','from-blue-100','via-sky-100','to-indigo-100'],
+}
